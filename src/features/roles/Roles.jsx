55fd60c731c5deeb2711cedd76a2d6f49ec030c9
encoderit-rafi <112605862+encoderit-rafi@users.noreply.github.com>
2025-01-13 15:48:13 +0600

    1-13-2025 stop unusual render Role.jsx

--- src/features/roles/Roles.jsx
+++ src/features/roles/Roles.jsx
@@ -1,20 +1,13 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import Title from "@/components/texts/Title";
 import BaseButton from "@/components/buttons/BaseButton";
 import Dialog from "@/components/dialogs/Dialog";
 import UserForm from "./components/userForm";
 import UserCard from "./components/UserCard";
 import UserCardSkeleton from "./components/UserCardSkeleton";
-import DownIcon from "@/assets/icons/DownIcon";
-import BaseInput from "@/components/inputs/BaseInput";
-import IconButton from "@/components/buttons/IconButton";
-import Label from "@/components/texts/Label";
 import { useGetAllUsers } from "./api/queries/useGetAllUsers";
-// import { useDebounce } from "react-use";
-import { debounce, PAGINATION } from "@/consts";
+import { PAGINATION } from "@/consts";
 import DialogConfirmDelete from "@/components/dialogs/DialogConfirmDelete";
-import { useDeleteUser } from "./api/mutations/useDeleteUser";
-import { useDebounce } from "@/hooks/useDebounce";
 import Pagination from "@/components/pagination";
 import { Axios } from "@/axios";
 import { useCallback } from "react";
@@ -27,20 +20,7 @@ export default function Roles() {
     isFetching: isFetchingAllUsers,
     params: paramsAllUsers,
     setParams: setParamsAllUsers,
-  } = useGetAllUsers({ setToUrl: true, isEnabled: false });
-  // const {
-  //   mutate: deleteUser,
-  //   isLoading: isLoadingDeleteUser,
-  //   isError: isErrorDeleteUser
-  // } = useDeleteUser({ setToUrl: true, isEnabled: false });
-
-  // useEffect(() => {
-  //   fetchAllUsers();
-  // }, []);
-  useEffect(() => {
-    fetchAllUsers();
-  }, [paramsAllUsers]);
-
+  } = useGetAllUsers({ setToUrl: true, isEnabled: true });
 
   const [isOpenCreateUser, setIsOpenCreateUser] = useState(false);
   const [isOpenUpdateUser, setIsOpenUpdateUser] = useState(false);
@@ -57,25 +37,35 @@ export default function Roles() {
     //     setIsOpenDeleteUser(false);
     //   },
     // });
-    setIsLoadingDeleteUser(true)
-    const res = await Axios.get(`/users/delete/${formValues?.user?.id}`)
+    setIsLoadingDeleteUser(true);
+    const res = await Axios.get(`/users/delete/${formValues?.user?.id}`);
     if (res.status === 200) {
-      setParamsAllUsers(paramsAllUsers)
+      setParamsAllUsers(paramsAllUsers);
       fetchAllUsers();
       setFormValues({ type: "create", user: null });
       setIsOpenDeleteUser(false);
     }
-    setIsLoadingDeleteUser(false)
+    setIsLoadingDeleteUser(false);
   }
   const handlePageChange = useCallback(
     (val) => setParamsAllUsers((old) => ({ ...old, page: val })),
     [setParamsAllUsers]
   );
-
   const handlePerPageChange = useCallback(
     (val) => setParamsAllUsers((old) => ({ ...old, page: 1, per_page: val })),
     [setParamsAllUsers]
   );
+
+  const paginationProps = useMemo(() => {
+    return {
+      to: allUsers?.to,
+      total: allUsers?.total,
+      current_page: allUsers?.current_page,
+      last_page: allUsers?.last_page,
+      per_page: allUsers?.per_page,
+    };
+  }, [allUsers]);
+
   return (
     <div className="flex flex-col h-full gap-4 overflow-hidden">
       <div className="flex items-center justify-between">
@@ -95,40 +85,36 @@ export default function Roles() {
       <div className="flex-1 overflow-auto">
         <div className="grid grid-cols-1 gap-6 mt-2 md:grid-cols-2 xl:grid-cols-3">
           {!isLoadingAllUsers &&
-            !isFetchingAllUsers &&
-            allUsers?.data?.length > 0
+          !isFetchingAllUsers &&
+          allUsers?.data?.length > 0
             ? allUsers.data.map((user) => (
-              <UserCard
-                key={user.id}
-                data={user}
-                onClickEdit={() => {
-                  setFormValues({ type: "update", user });
-                  setIsOpenUpdateUser(true);
-                }}
-                onClickDelete={() => {
-                  setFormValues({ type: "delete", user });
-                  setIsOpenDeleteUser(true);
-                }}
-              />
-            ))
+                <UserCard
+                  key={user.id}
+                  data={user}
+                  onClickEdit={() => {
+                    setFormValues({ type: "update", user });
+                    setIsOpenUpdateUser(true);
+                  }}
+                  onClickDelete={() => {
+                    setFormValues({ type: "delete", user });
+                    setIsOpenDeleteUser(true);
+                  }}
+                />
+              ))
             : Array.from({ length: PAGINATION.per_page }, (_, i) => (
-              <UserCardSkeleton key={i} />
-            ))}
+                <UserCardSkeleton key={i} />
+              ))}
         </div>
       </div>
 
       {allUsers?.total > 0 && (
         <Pagination
-          to={allUsers?.to}
-          total={allUsers?.total}
-          current_page={allUsers?.current_page}
-          last_page={allUsers?.last_page}
-          per_page={allUsers?.per_page}
+          {...paginationProps}
           onPageChange={handlePageChange}
           onPerPageChange={handlePerPageChange}
         />
-
       )}
+
       <Dialog
         isOpen={isOpenCreateUser}
         title="add new user"
